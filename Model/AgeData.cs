2c506648a13eedbc64eb8109c5f97c45f3ae8508
LilianDyer <lmd@pobox.com>
2025-01-09 11:22:16 -0700

    Add project files.

---
+++ Model/AgeData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCodeChallenge.Model
+{
+    public class AgeData
+    {
+        public required string Data { get; set; }
+
+        public int Count()
+        {
+            var dataArr = Data.Split(',');
+            var ages = dataArr.ToList().FindAll(x => x.Contains("age="));
+            var agesInt = ages.Select(x => int.Parse(x.Split('=')[1])).ToList();
+            int count = agesInt.Count(x => x >= 50);
+     
+            return count;
+        }
+
+        public int GetCount() {
+            return Count();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Newtonsoft.Json;
+using StringCodeChallenge.Model;
+
+var client = new HttpClient();
+var response = await client.GetAsync("https://coderbyte.com/api/challenges/json/age-counting");
+var data = await response.Content.ReadAsStringAsync();
+var ageData = JsonConvert.DeserializeObject<AgeData>(data);
+Console.WriteLine(ageData?.GetCount());
